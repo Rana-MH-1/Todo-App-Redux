674f89205b8c2927495e8cb776879ed09e1540df
Rana-MH-1 <rana.meftah@enis.tn>
2021-06-07 15:51:59 +0100

    adding todoapp files

---
+++ src/Reducers/FilterReducer.js
@@ -0,0 +1,11 @@
+
+
+export const FilterReducer = (state= '', action)=>{
+
+    switch(action.type){
+        case 'FILTER_IN_STATE' :
+            return {...state,filter: action.payload};
+        default:
+            return state;
+    }
+}
